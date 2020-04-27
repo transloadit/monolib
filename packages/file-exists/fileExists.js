4ccd6f09aa9d8aadb2b26b4148f9696d49e76d55
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-04-27 15:43:54 +0200

    Yarn

--- packages/file-exists/fileExists.js
+++ packages/file-exists/fileExists.js
@@ -1,4 +1,4 @@
-const fs             = require('fs')
+const fs = require('fs')
 module.exports = function fileExists (s) {
   return new Promise((resolve, reject) => {
     fs.access(s, fs.F_OK, (err) => {
