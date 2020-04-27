50623234aedba1ab456f3e6e4dba371205307655
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-04-27 15:15:45 +0200

    Init lerna

--- simple/file-exists.js
+++
@@ -1,8 +0,0 @@
-const fs             = require('fs')
-module.exports = function fileExists (s) {
-  return new Promise((resolve, reject) => {
-    fs.access(s, fs.F_OK, (err) => {
-      resolve(!err)
-    })
-  })
-}
