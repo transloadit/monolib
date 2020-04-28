b7397d47921e7c28626b05ab2c0bffcce3071897
Kevin van Zonneveld <kevin@vanzonneveld.net>
2020-04-28 20:52:00 +0200

    post: Automatically titlecase and open correct editor

--- packages/post/README.md
+++ packages/post/README.md
@@ -1,3 +1,31 @@
 > Even though this module is publicly accessible, we do not recommend using it in projects outside of Transloadit. We won't make any guarantees about its workings and can change things at any time, we won't adhere strictly to SemVer.
 
 > This module is maintained from a private mono repo called monolib.
+
+
+## Install
+
+```bash
+sudo yarn global add @transloadit/post
+```
+
+## Usage
+
+```bash
+$ post
+Welcome to @transloadit/post@0.0.21. 
+Please answer some questions about the blog post, 
+and I'll generate a starting point and open your editor. 
+? title: a fresh look at tus.io
+? author: kvz
+Opening'/home/kvz/code/content/_posts/2020-04-28-a-fresh-look-at-tus-io.md' in your editor .. 
+Done. 
+```
+
+![image](https://user-images.githubusercontent.com/26752/80525678-e4cb1880-8991-11ea-9266-d28b884e5c35.png)
+
+Following values can be used to detect code editor (in descending priority):
+
+- `process.env.OPEN_FILE`
+- `process.env.VISUAL`
+- `process.env.EDITOR`
--- packages/post/post.js
+++ packages/post/post.js
@@ -1,28 +1,78 @@
 #!/usr/bin/env node
 const fs = require('fs').promises
 const inquirer = require('inquirer')
+const openInEditor = require('open-in-editor')
 const fileExists = require('@transloadit/file-exists')
 const slugify = require('@transloadit/slugify')
+const title = require('title')
 
 async function post () {
+  console.log(`Welcome to @transloadit/post@${require(`${__dirname}/package.json`).version}. `)
+  console.log(`Please answer some questions about the blog post, `)
+  console.log(`and I'll generate a starting point and open your editor. `)
+
   const postDir = `${process.cwd()}/${process.argv[2] || '_posts'}`
-  if (!(await fileExists(`${postDir}`))) {
+  if (!(await fileExists(postDir))) {
     throw new Error(`Dir does not exist: '${postDir.replace(process.cwd(), '.')}'`)
   }
 
   const mysqlNow = (new Date()).toISOString().replace('T', ' ').split('.')[0].split(' ')[0]
+  // eslint-disable-next-line no-unused-vars
+  const [dateY, datem, dated] = mysqlNow.split('-')
   const answers = await inquirer
     .prompt([
-      { type: 'input', name: 'title', message: 'title' },
-      { type: 'input', name: 'author', message: 'author', default: process.env.USER },
+      { type: 'input', name: 'title', message: 'title:' },
+      { type: 'input', name: 'author', message: 'author:', default: process.env.USER },
     ])
 
   const outFile = `${postDir}/${mysqlNow}-${slugify(answers.title)}.md`
-  const buff = `---\ntitle: ${answers.title}\nauthor: ${answers.author}\n---\n\n`
+
+  if ((await fileExists(outFile))) {
+    throw new Error(`File already exist: '${outFile.replace(process.cwd(), '.')}'`)
+  }
+
+  const strTitle = title(answers.title, {
+    special: [
+      'tus',
+      'io',
+    ],
+  })
+
+  let buff = ''
+  buff += `---\n`
+  buff += `title: ${strTitle}\n`
+  buff += `author: ${answers.author}\n`
+  buff += `ogimage: /assets/images/blog/${dateY}-${datem}-x.jpg\n`
+  buff += `tags: \n`
+  buff += `  - \n`
+  buff += `meta_description: >-\n`
+  buff += `  \n`
+  buff += `---\n`
+  buff += `\n`
   await fs.writeFile(outFile, buff, 'utf-8')
-  console.log(`\n`)
-  console.log(`code '${outFile}'`)
-  console.log(`\n`)
+
+  console.log(`Opening'${outFile}' in your editor .. `)
+
+  // Avoid crashing on /bin/sh: 1: code -w: not found
+  // strip any arguments
+  if (process.env.VISUAL) {
+    process.env.VISUAL = process.env.VISUAL.split(/\s+/)[0]
+  }
+
+  let opts = {}
+
+  if (process.env.VISUAL === 'code') {
+    opts = {
+      cmd    : 'code',
+      pattern: '-r -g {filename}:{line}:{column}',
+    }
+  }
+
+  const editor = openInEditor.configure(opts)
+  await editor.open(`${outFile}`, {
+    line: 10,
+  })
+  console.log(`Done. `)
 }
 post().catch(err => {
   console.error(err)
