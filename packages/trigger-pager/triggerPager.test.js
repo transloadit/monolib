b746cd875d9b9965a191ebe72370c9a7222f0d42
Kevin van Zonneveld <kevin@vanzonneveld.net>
2022-02-09 14:17:25 +0100

    Add expectation on the payload we send out

--- packages/trigger-pager/triggerPager.test.js
+++ packages/trigger-pager/triggerPager.test.js
@@ -7,13 +7,14 @@ describe('triggerPager', () => {
   test('main', async () => {
     const post = jest.fn(async (endpoint, payload) => {
       expect(endpoint).toBe('/incidents')
-      expect(payload).toBeTruthy()
+      expect(payload).toMatchSnapshot()
       return { data: { error: null } }
     })
     api.mockReturnValue({ post })
     await triggerPager({
-      title      : '',
-      description: '',
+      title      : 'us-east-1 down',
+      description: 'this happens every 2 years',
+      serviceId  : 'SERVICE_ID',
     })
     expect(post.mock.calls.length).toBe(1)
   })
