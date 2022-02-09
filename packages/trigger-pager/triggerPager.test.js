7db5cb4a0f7976958006650a93221a6b53790371
Kevin van Zonneveld <kevin@vanzonneveld.net>
2022-02-09 14:22:02 +0100

    Test abbreviation

--- packages/trigger-pager/triggerPager.test.js
+++ packages/trigger-pager/triggerPager.test.js
@@ -1,6 +1,16 @@
 const { api } = require('@pagerduty/pdjs')
 const triggerPager = require('./triggerPager')
 
+const LOREM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.`
+
+const LOREM3 = `${LOREM} ${LOREM} ${LOREM}`
+
 jest.mock('@pagerduty/pdjs')
 
 describe('triggerPager', () => {
@@ -12,8 +22,8 @@ describe('triggerPager', () => {
     })
     api.mockReturnValue({ post })
     await triggerPager({
-      title      : 'us-east-1 down',
-      description: 'this happens every 2 years',
+      title      : LOREM3,
+      description: LOREM3,
       serviceId  : 'SERVICE_ID',
     })
     expect(post.mock.calls.length).toBe(1)
