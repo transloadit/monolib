2cd29a53972e8b06f9ae1b13e6dcd22dd5187c99
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-10-31 15:33:02 +0100

    Linting fixes

--- packages/prettier-bytes/src/prettierBytes.ts
+++ packages/prettier-bytes/src/prettierBytes.ts
@@ -1,8 +1,8 @@
 // Adapted from https://github.com/Flet/prettier-bytes/
 // Changing 1000 bytes to 1024, so we can keep uppercase KB vs kB
 // ISC License (c) Dan Flettre https://github.com/Flet/prettier-bytes/blob/master/LICENSE
-export default function prettierBytes(num: $TSFixMe) {
-  if (typeof num !== 'number' || isNaN(num)) {
+export default function prettierBytes(num: number) {
+  if (typeof num !== 'number' || Number.isNaN(num)) {
     throw new TypeError(`Expected a number, got ${typeof num}`)
   }
 
@@ -18,7 +18,7 @@ export default function prettierBytes(num: $TSFixMe) {
   }
 
   const exponent = Math.min(Math.floor(Math.log(num) / Math.log(1024)), units.length - 1)
-  num = Number(num / Math.pow(1024, exponent))
+  num = Number(num / 1024 ** exponent)
   const unit = units[exponent]
 
   if (num >= 10 || num % 1 === 0) {
