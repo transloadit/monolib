32c3e7f583f1b05b1103b69bdd36247258a17c89
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-11-01 16:17:17 +0100

    Passing test

--- packages/has-property/src/has-property.ts
+++ packages/has-property/src/has-property.ts
@@ -3,5 +3,5 @@ export function hasProperty<K extends PropertyKey>(
   obj: unknown,
   key: K | null | undefined
 ): obj is Record<K, unknown> {
-  return key != null && obj != null && typeof obj === 'object' && key in obj
+  return key != null && obj != null && typeof obj === 'object' && Object.hasOwn(obj, key)
 }
