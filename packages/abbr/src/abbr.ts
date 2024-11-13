c3fe11b4bc660f1c79f1bc1e94c4fe638570c136
Kevin van Zonneveld <kevin@vanzonneveld.net>
2024-11-13 12:38:41 +0100

    Remove deprecated substr

--- packages/abbr/src/abbr.ts
+++ packages/abbr/src/abbr.ts
@@ -7,9 +7,7 @@ export = function abbr(str: string, maxLength = 55, divider = `[...]`): string {
   if (abbreviated.length > maxLength) {
     const firstLen = Math.ceil((maxLength - divider.length) / 2)
     const lastLen = Math.floor((maxLength - divider.length) / 2)
-    abbreviated = `${abbreviated.substr(0, firstLen)}${divider}${abbreviated.substr(
-      abbreviated.length - lastLen,
-    )}`
+    abbreviated = `${abbreviated.slice(0, firstLen)}${divider}${abbreviated.slice(-lastLen)}`
   }
 
   return abbreviated
